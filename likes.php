e8165f796fec500c80b42a547e14a5638e6177b9
Hass <seennd1@gmail.com>
2022-05-26 19:45:51 +0300

    like.php done

--- likes.php
+++ likes.php
@@ -1,36 +1,44 @@
-<?php if (isset($_GEt['id'])){
+<?php
 session_start();
 require "connection.php";
+
+
 $email=$_SESSION['email'];
-$id =(int)$_GEt['id'];
+$id =(int) $_GET['id'];
  
-conn->query("
-	INSERT INTO post_likes (user,post)
-		SELECT {$email},{$id}
-		FROM posts 
-		WHERE EXISTS (
-
-			SELECT id
-			FROM posts 
-			WHERE id={$id})
-		AND NOT EXISTS (
-			SELECT id 
-			FROM post_likes
-			WHERE user={$email})
-			AND post={$id})
-		LIMIT 1
-
-	")
+   $sql= "INSERT INTO post_likes (email,post) VALUES(?,?)";
+     $stmt = $conn->prepare($sql); 
+     $stmt->bind_param("si", $email,$id);
+     $stmt->execute();
+     $result = $stmt->get_result();
 
 
+if ($conn -> affected_rows>0) {
+	$x="UPDATE posts SET likes = likes + 1 WHERE id = ? ";
+	$s = $conn->prepare($x); 
+    $s->bind_param("i", $id);
+    $s->execute();
+}else{
+	$sq = "DELETE FROM post_likes WHERE email= ? AND post=? ";
+	$st = $conn->prepare($sq); 
+    $st->bind_param("si", $email,$id);
+    $st->execute();
+
+
+	$z="UPDATE posts SET likes = likes - 1 WHERE id = ? ";
+	$f = $conn->prepare($z); 
+    $f->bind_param("i", $id);
+    $f->execute();
+
+
+}
 
 
 
-} 
 
-
-				header('Location:http://localhost/web%20project/home.php');
-                die;
+ header('Location:http://localhost/web%20project/home.php');
+     die;
 
 ?>
+
 	
